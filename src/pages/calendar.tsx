559f7d75544e42e6142d44650ffdf168a0718ef5
Juliana Soba <73260408+JulianaSobaJava@users.noreply.github.com>
2026-04-28 14:36:38 +0100

    style: remove unecessary imports

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -1,6 +1,5 @@
 import CalendarUI from "@/components/calendar";
 import Heading from "@/components/heading";
-import { DataTableDemo } from "@/components/table";
 import { Button } from "@/components/ui/button";
 import { Card} from "@/components/ui/card";
 import {  Plus, RefreshCcw, Wrench } from "lucide-react";
@@ -14,7 +13,7 @@ export default function Calendar() {
       </Heading>
           <div className="grid grid-cols-4 py-6 gap-6 ">
           <Card className="p-4 rounded-xs col-span-3">
-    <DataTableDemo/>
+   {/* <EmployeeTable data={[]} />*/}
     </Card>
     <div className="flex flex-col items-end">
     <CalendarUI classname="w-full"/>
