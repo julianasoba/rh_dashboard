e1f830b97d3628f55ce6637af9f6fca0f2a68389
Juliana Soba <73260408+JulianaSobaJava@users.noreply.github.com>
2026-04-28 14:37:30 +0100

    feat: add protected routes and auth

--- src/layout/protectedroutes.tsx
+++ src/layout/protectedroutes.tsx
@@ -1,10 +1,16 @@
-import { useAuth } from '@/hooks/useAuth';
-import { Navigate, Outlet } from 'react-router';
+import Loader from "@/components/loader";
+import { useAuth } from "@/hooks/useAuth";
+import { Navigate, Outlet } from "react-router";
 
 export default function ProtectedRoute() {
   const { user, isLoading } = useAuth();
 
-  if (isLoading) return <div>A carregar...</div>;
+  if (isLoading)
+    return (
+      <div className="w-screen h-screen flex justify-center items-center">
+        <Loader />
+      </div>
+    );
   if (!user) return <Navigate to="/login" replace />;
 
   return <Outlet />;
